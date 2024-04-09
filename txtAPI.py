e57d10bfeb575cea48e1cd11da2500bbaff381e4
Kepler <olmancastro@estudiantec.cr>
2024-04-09 01:14:08 -0600

    Update conversacion.json and txtAPI.py

--- txtAPI.py
+++ txtAPI.py
@@ -2,10 +2,9 @@ from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 import os
 import tempfile
-from google.cloud import language_v1beta2
+from google.cloud import language_v2
 import json
 import re
-from google.cloud import language_v1beta2
 
 def sample_classify_text(input_data):
     """
@@ -15,31 +14,21 @@ def sample_classify_text(input_data):
       input_data: A dictionary with the structure {"description": "text to analyze"}.
     """
     # Initialize the Google Cloud client
-    client = language_v1beta2.LanguageServiceClient()
-    print(input_data)
+    client = language_v2.LanguageServiceClient()
     # Extract text_content from input_data
     text_content = input_data.get("description", "")
-
+    print(text_content)
     # Define document type and language
-    type_ = language_v1beta2.Document.Type.PLAIN_TEXT
-    language = "es"
-    document = {"content": text_content, "type": type_, "language": language}
+    type_ = language_v2.Document.Type.PLAIN_TEXT
+    document = {"content": text_content, "type": type_}
     # Specify model version for content categorization
-    content_categories_version = language_v1beta2.ClassificationModelOptions.V2Model.ContentCategoriesVersion.V2
 
-    # Call the classify_text method
-    response = client.classify_text(
-        request={
-            "document": document,
-            "classification_model_options": {
-                "v2_model": {"content_categories_version": content_categories_version}
-            },
-        }
-    )
+    response = client.classify_text(request={'document': document})
 
     # Process and return the response categories
     categories = [{"name": category.name, "confidence": category.confidence} for category in response.categories]
     return categories
+
 
 def txt_to_json_messages_only(filepath, lang='auto'):
     """
@@ -64,9 +53,10 @@ def txt_to_json_messages_only(filepath, lang='auto'):
                     lang = 'es'
                     break
     
+    # Actualizado para capturar ambos formatos de fecha: 'dd/mm/yy' y 'm/dd/yy'
     pattern = {
-        'en': r'(\d{2}/\d{2}/\d{2}), (\d{1,2}:\d{2}\s[ap]m) - (.*?): (.*)',
-        'es': r'(\d{2}/\d{2}/\d{2}), (\d{1,2}:\d{2}\s[a|p]\. m\.) - (.*?): (.*)'
+        'en': r'(\d{1,2}/\d{1,2}/\d{2}), (\d{1,2}:\d{2}\s[ap]m) - (.*?): (.*)',
+        'es': r'(\d{1,2}/\d{1,2}/\d{2}), (\d{1,2}:\d{2}\s[a|p]\.m\.) - (.*?): (.*)'
     }[lang if lang in ['en', 'es'] else 'en']
     
     with open(filepath, 'r', encoding='utf-8') as file:
@@ -74,16 +64,13 @@ def txt_to_json_messages_only(filepath, lang='auto'):
             match = re.match(pattern, line)
             if match:
                 _, _, _, message = match.groups()
-                if "end-to-end encrypted" not in message and "cifrado de extremo a extremo" not in message and "(file attached)" not in message:
-                    print("Mostar mensaje: ", message)
+                if "end-to-end encrypted" not in message and "cifrado de extremo a extremo" not in message and "<Media omitted>" not in message:
                     messages.append(message.replace("<This message was edited>", "").strip())
+    
     # Concatenar todos los mensajes en un único string
     conversation = ' '.join(messages)
-    print("Conversación: ", conversation)
-    # Devolver el contenido en el formato esperado para sample_classify_text
+    # Devolver el contenido en el formato esperado
     return json.dumps({"description": conversation}, ensure_ascii=False, indent=2)
-
-
 
 app = Flask(__name__)
 
@@ -106,7 +93,6 @@ def analyze_chat():
                 processed_content = json.load(f)
         else:
             return jsonify({'error': 'Unsupported file type'}), 400
-
 
     # Realiza la clasificación del texto
     categories = sample_classify_text(processed_content)
